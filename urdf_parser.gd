f8316dbd8d4de0337731e1afaf5c32a71757700c
Difint <yarm-public@protonmail.com>
2023-05-13 18:46:55 +0700

    - urdf parser

--- urdf_parser.gd
+++ urdf_parser.gd
@@ -16,19 +16,61 @@ func parse(source: String) -> Node3D:
 	while parser.read() == OK:
 		if parser.get_node_type() == XMLParser.NODE_ELEMENT:
 			var element = parser.get_node_name()
-			if element == "link":
-				# Handle the link element
-				print("link")
-				var name = parser.get_named_attribute_value("name")
-				
-				var link_node = create_link_node(name)
-				root.add_child(link_node)
-				link_node.owner = root
-				
-			elif element == "joint":
-				# Handle the joint element
-				print("joint")
-				pass
+			
+			match element:
+				"link":
+					# Handle the link element
+	#				print("link")
+					var name = parser.get_named_attribute_value("name")
+					
+					var link_node = create_link_node(name)
+					root.add_child(link_node)
+					link_node.owner = root	
+				"joint":
+					# Handle the joint element
+#					print("joint")
+					pass
+				"robot":
+					root.name = parser.get_named_attribute_value("name")
+				"visual":
+					var count = root.get_child_count()
+					var parent = root.get_child(count-1)
+					
+					var visual = Node3D.new()
+					
+					parent.add_child(visual)
+					visual.owner = root
+					
+					print("visual")
+					
+					print(parent, visual)
+				"mesh":
+					var filename = parser.get_named_attribute_value("filename")
+					
+					var p = "res://addons/nasa/m2020-urdf-models/rover/" + filename.right(filename.length()-2)
+					
+					print("source path: ", source)
+					print("filename path: ", filename)
+					print("res path: ", p)
+					
+					var res = load(p)
+					print("Res result: ", res)
+					
+					var mesh = res.instantiate()
+#					mesh.load
+					print("Mesh filename: ", filename)
+					
+					
+					var count = root.get_child_count()
+					var parent = root.get_child(count-1)
+					
+					
+					parent.add_child(mesh)
+					mesh.owner = root
+					
+					
+				_:
+					print("Unknown element: ", element)
 	
 	return root
 	
